62e43f8b852cfc41bdf550347c3d0402b9b93a35
Lucas <lucascostamr812@gmail.com>
2022-10-22 17:44:55 -0300

    Front End almost finished and starting Back End

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
